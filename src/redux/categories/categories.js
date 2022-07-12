561ec18efe4f0256fb3c432afd8f27af86cc394b
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-07-12 11:11:24 -0600

    Configure reducers and actions for books and status

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,21 @@
+// Create Const for the Actions
+const CHECKING_STATUS = 'bookstore/books/CHECKING_STATUS';
+
+// Create function to check status
+const checkingStatus = () => ({
+  type: CHECKING_STATUS,
+});
+
+// Reducer for the status
+const statusReducer = (state = [], action) => {
+  switch (action.type) {
+    case CHECKING_STATUS:
+      return 'Under Construction';
+    
+    default:
+      return state;
+  }
+}
+
+export { checkingStatus };
+export default statusReducer;
---
+++ src/redux/configureStore.js
@@ -0,0 +1,14 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import statusReducer from './categories/categories';
+import booksReducer from './books/books';
+
+// Establish the root reducer
+const rootReducer = combineReducers({
+  booksReducer,
+  statusReducer
+});
+
+// Set up the store
+const store = configureStore(rootReducer);
+
+export default store;
